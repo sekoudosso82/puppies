8764ab4b06acca8a8345ea71613fd91def87036f
YOUR NAME <YOUR@EMAIL.com>
2021-03-11 13:04:09 -0500

    tests for new owner/create owner

--- test/controllers/admin/owners_controller_test.rb
+++ test/controllers/admin/owners_controller_test.rb
@@ -19,6 +19,23 @@ class Admin::OwnersControllerTest < ActionDispatch::IntegrationTest
   test "should show admin_owner" do
     get admin_owner_url(@owner)
     assert_response :success
+  end
+  
+  #  new owner 
+  test " new owner" do 
+    get new_admin_owner_url
+    assert_response :success
   end 
+  # after new  owner , create new owner
+  setup do
+    @new_owner =  { email: "sekou@gmail.com", first_name: "Sekou", last_name: "Dosso", phone_number: 234 } 
+  end 
+  test "create new owner" do
+    assert Owner.create(@new_owner)
+    assert owner = Owner.new(@new_owner)
+    assert owner.invalid?
+    assert owner.errors.include?(:email)
+  end 
+
 
 end
