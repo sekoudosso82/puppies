ef09847d5ead6b134dfa2c22c8b7c9189030db75
YOUR NAME <YOUR@EMAIL.com>
2021-03-11 11:47:25 -0500

    first test get all owners

--- test/controllers/owners_controller_test.rb
+++ test/controllers/owners_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class OwnersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  # test "title" do 
+  #   body 
+  #   assert_response :success
+    
+  # end 
+
+  test "get all owners" do 
+    get admin_owners_url
+    assert_response :success 
+  end
+
+  
+
 end
